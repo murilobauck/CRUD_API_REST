ab5c9d271e40658cad38aa52d241ad6f4e0643d6
murilobauck <murilobauck2@gmail.com>
2026-03-16 20:13:37 -0300

    feat: setup project structure with src/servers, src/screens, and src/styles

---
+++ src/screens/AddEditScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AddEditScreen = () => {
+    return (
+        <View>
+            <Text>Add/Edit Screen</Text>
+        </View>
+    );
+};
+
+export default AddEditScreen;
